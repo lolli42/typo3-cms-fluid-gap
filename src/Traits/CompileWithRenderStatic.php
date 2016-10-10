3928a79638ce086369e3004b866d717d6c6112d2
Claus Due <claus@namelesscoder.net>
2016-10-10 19:37:22 +0200

    [TASK] Initial commit using class aliases

---
+++ src/Traits/CompileWithRenderStatic.php
@@ -0,0 +1,6 @@
+<?php
+namespace NamelessCoder\FluidGap\Traits;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class_alias(CompileWithRenderStatic::class, 'NamelessCoder\\FluidGap\\Traits\\CompileWithRenderStatic');
